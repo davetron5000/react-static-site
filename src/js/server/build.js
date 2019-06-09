7b6de94fb9e5ada949f484be0071ee92acf3995d
Dave Copeland <david.copeland@livingsocial.com>
2019-06-09 13:25:28 -0400

    reorg

--- src/js/server/build.js
+++ src/js/server/build.js
@@ -8,10 +8,20 @@ require("@babel/register")({
 });
 
 const path = require("path");
-
+const fs = require("fs");
 const render = require("./render").default;
+const log = require("./log").default;
 
+const source_path = path.join(__dirname, "..", "..", "..", "site");
+const output_path = path.join(__dirname, "..", "..", "..", "site", "rendered_html");
+
+log(`source_path is '${source_path}'`);
+log(`output_path is '${output_path}'`);
+
+fs.mkdirSync(output_path, { recursive: true });
+
+log("Rendering");
 render({
-  source_path: path.join(__dirname, "..", "..", "html"),
-  output_path: path.join(__dirname, "..", "..", "..", "site")
+  source_path: source_path,
+  output_path: output_path
 });
